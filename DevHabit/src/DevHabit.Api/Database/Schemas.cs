60dae6e8d7db8a8f63d123088a84deac03e953ba
alonsokratos31 <alonso.garcia025@gmail.com>
2025-12-18 17:57:31 -0300

    Agregar configuración de base de datos y entidades para la API de DevHabit

---
+++ DevHabit/src/DevHabit.Api/Database/Schemas.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevHabit.Api.Database;
+
+public static class Schemas
+{
+    public const string Application = "dev_habit";
+}
+
--- DevHabit/src/DevHabit.Api/Program.cs
+++ DevHabit/src/DevHabit.Api/Program.cs
@@ -1,12 +1,23 @@
+using DevHabit.Api.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Data;
 using OpenTelemetry;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+using DevHabit.Api.Extensions;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options
+        .UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"),
+                   npgsqlOptions => npgsqlOptions
+                   .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
+        .UseSnakeCaseNamingConvention());
+
 
 builder.Services.AddOpenTelemetry()
     .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
@@ -30,6 +41,7 @@ WebApplication app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    await app.ApplyMigrationsAsync();
 }
 
 app.UseHttpsRedirection();
